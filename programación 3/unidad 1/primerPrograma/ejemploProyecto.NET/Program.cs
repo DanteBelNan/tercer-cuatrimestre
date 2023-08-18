4553efeb2d7bf702da0e7d948f1529b316ba83ce
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-08-18 00:01:21 -0300

    Bases de C# finalizado

--- programación 3/unidad 1/primerPrograma/ejemploProyecto.NET/Program.cs
+++ programación 3/unidad 1/primerPrograma/ejemploProyecto.NET/Program.cs
@@ -11,6 +11,21 @@ namespace ejemploProyecto.NET
 {
     internal class Program
     {
+        
+        static void Main(string[] args)
+        {
+            lecturaVariables(false);
+            condicionales(false);
+            ciclos(false);
+            vectores(false);
+            int x = 3;
+            pasajeReferencia(ref x);
+            Console.WriteLine(x);
+
+            Console.ReadKey();
+
+        }
+
         static void lecturaVariables(bool seHace)
         {
             if (seHace)
@@ -26,10 +41,9 @@ namespace ejemploProyecto.NET
                 f = d * d / 2;
 
                 Console.WriteLine("El Resultado es: " + c);
-                Console.ReadKey();
+
             }
         }
-
         static void condicionales(bool seHace)
         {
             if (seHace)
@@ -37,16 +51,94 @@ namespace ejemploProyecto.NET
                 int a, b, c;
                 a = 12;
                 b = 10;
+                c = 11;
                 if (a == b)
                 {
                     Console.WriteLine("SON IGUALES");
                 }
+                else if (a == c)
+                {
+                    Console.WriteLine("UNO ES IGUAL");
+                }
+                else
+                {
+                    Console.WriteLine("Nada es igual");
+                }
+
+                switch (a)
+                {
+                    case 1:
+
+                        break;
+
+                    case 2:
+
+                        break;
+
+                    case 3:
+
+                        break;
+
+                    case 12:
+                        Console.WriteLine("Se activa el switch");
+                        break;
+                    default:
+
+                        break;
+
+                }
+            }
+        }
+        static void ciclos(bool seHace)
+        {
+            if (seHace)
+            {
+                Console.WriteLine("For");
+                for (int x = 0; x < 10; x++)
+                {
+                    Console.WriteLine(x);
+                }
+
+                Console.WriteLine("While");
+                int y = 5;
+                while (y > 0)
+                {
+                    Console.WriteLine(y);
+                    y--;
+                }
+
+                Console.WriteLine("Do While");
+                do
+                {
+                    Console.WriteLine(y);
+                    y++;
+                } while (y < 5);
+
+            }
+        }
+        static void vectores(bool seHace)
+        {
+            if (seHace)
+            {
+                int[] vector = new int[10];
+
+                int x = 3;
+                for (int i = 0; i < 10; i++)
+                {
+                    x *= i + 1;
+                    vector[i] = x;
+                }
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(vector[i]);
+                }
             }
         }
 
-        static void Main(string[] args)
+        static void pasajeReferencia(ref int a)
         {
-            lecturaVariables(false);
+            a += 1;
         }
     }
 }
