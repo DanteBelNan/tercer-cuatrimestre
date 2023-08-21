130edd42c11771b45651ac125eb5115e0ab1b258
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-08-21 20:49:27 -0300

    Progra 3, objetos terminado

---
+++ programación 3/unidad 2/POO1/ejemplo 2/Articulo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo_2
+{
+    internal class Articulo
+    {
+        //Nueva forma de declarar variables con getters y setters
+        public int CodigoArticulo { get; set; }
+
+        public float Precio { get; set; }
+
+        //Codigo marca debe ser del 1 al 10
+        //Al tener logica propia, debemos customizar el set, por lo que no podemos
+        //utilizar estos nuevos setters y getters
+
+        private int codMarca;
+
+        public int CodigoMarca
+        {
+            get { return codMarca; }
+            set 
+            { 
+                if(value > 0 && value <= 10)
+                {
+                    codMarca = value;
+                }
+                else
+                {
+                    codMarca = -1;
+                }
+            }
+        }
+
+        public void Mostrar()
+        {
+            Console.WriteLine("Codigo Articulo: " + CodigoArticulo.ToString());
+            Console.WriteLine("Precio: " +  Precio.ToString());
+            Console.WriteLine("Codigo Marca: " + CodigoMarca.ToString());
+        }
+
+    }
+}
---
+++ programación 3/unidad 2/POO1/ejemplo 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Articulo[] articulos = new Articulo[3];
+
+            for(int x = 0; x < articulos.Length; x++)
+            {
+                articulos[x] = new Articulo();
+                Console.WriteLine("Articulo: " + x);
+                Console.WriteLine("Codigo: ");
+                articulos[x].CodigoArticulo = int.Parse(Console.ReadLine());
+                Console.WriteLine("Precio: ");
+                articulos[x].Precio = float.Parse(Console.ReadLine());
+                Console.WriteLine("Codigo de Marca (1 al 10): ");
+                articulos[x].CodigoMarca = int.Parse(Console.ReadLine());
+            }
+
+
+            for(int x = 0;x < articulos.Length; x++)
+            {
+                articulos[x].Mostrar();
+            }
+            Console.ReadKey();
+        }
+    }
+}
