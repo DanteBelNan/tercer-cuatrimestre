d9865765e69e2ce854d2ac6ae6a06dd6913583e8
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-08-21 21:46:41 -0300

    Progra 3, Constructores y destructores

---
+++ programación 3/unidad 2/Constructores y Destructores/Constructores y Destructores/Botella.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructores_y_Destructores
+{
+    internal class Botella
+    {
+        public Botella(string color, string material) {
+            this.color = color;
+            this.material = material;
+        } //El constructor puede ser sobrecargado
+
+        public Botella()
+        {
+            Console.WriteLine("Constructor sin parametros");
+        }
+
+        ~Botella()
+        {
+            //Logica para un destructor, no es public ni private
+        }
+        private int capacidad;
+        private string color;
+        private string material;
+
+
+        public int Capacidad
+        {
+            get { return capacidad; }
+            set { capacidad = value; }
+        }
+
+        public int Material
+        {
+            get { return material; }
+        }
+    }
+}
---
+++ programación 3/unidad 2/Constructores y Destructores/Constructores y Destructores/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructores_y_Destructores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Botella b1 = new Botella("Rojo" , "Plastico");
+            b1.Capacidad = 10;
+
+            Console.ReadKey();
+        }
+    }
+}
