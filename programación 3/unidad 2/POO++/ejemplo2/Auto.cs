f77846ed1a6658de6d058d940593b74d781d76f7
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-08-23 16:58:58 -0300

    Progra 3 Composición y agregación

--- programación 3/unidad 2/POO++/ejemplo2/Auto.cs
+++ programación 3/unidad 2/POO++/ejemplo2/Auto.cs
@@ -8,5 +8,18 @@ namespace ejemplo2
 {
     internal class Auto : Vehiculo
     {
+        public Auto() {
+            BaseAuto = new BaseAuto();
+        }
+        public int Year {  get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+
+        //Composición;
+        BaseAuto BaseAuto { get; }
+        //Es obligatorio que tenga un chasis, y este no puede ser mutable
+        //Agregación
+        public Motor Motor { get; set; }
+        //Pueden ser mutable el motor, puede estar como no estar
     }
 }
