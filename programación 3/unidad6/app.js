8660c31f85210004cfcdcf4bb6af0b9c59a13eb1
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-10-03 20:04:55 -0300

    progra3 unidad 6 updates js y css

---
+++ programación 3/unidad6/app.js
@@ -0,0 +1,7 @@
+function login(){
+    let user = document.getElementById("user").value;
+    let pass = document.getElementById("pass").value;
+    let login = document.getElementById("login");
+
+    alert("Usuario: " + user + " | Password: " +  pass)
+}
