1021b75166a9e7f7f4c400c4d7e0ad80da291849
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-10-04 23:36:58 -0300

    progra3 portfolio terminado

--- programación 3/unidad6/portfolio/app.js
+++ programación 3/unidad6/portfolio/app.js
@@ -4,3 +4,10 @@ function shDescription(){
     desc.classList.toggle("hidden");
     desc.classList.toggle("shown");
 }
+
+function sendMessage(){
+    let email = document.getElementById("email").value;
+    let msj = document.getElementById("msj").value;
+
+    alert("Correo de " + email + " enviado, con el mensaje: " + msj);
+}
